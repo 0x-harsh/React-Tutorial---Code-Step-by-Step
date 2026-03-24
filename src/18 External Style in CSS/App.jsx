216ee09fb9250cc7c5e68e64bd1814b1952bee07
Harsh Dhiman <harsh61355@gmail.com>
2026-03-24 22:45:33 +0530

    External style in ReactJS

---
+++ src/18 External Style in CSS/App.jsx
@@ -0,0 +1,11 @@
+import './style.css'
+
+const App = () => {
+    return (
+        <div>
+            <div className='heading'>External Style</div>
+        </div>
+    )
+}
+
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './17 Inline Style in ReactJS/App.jsx'
+import App from './18 External Style in CSS/App.jsx'
 
 createRoot(document.getElementById('root')).render(<App />)
