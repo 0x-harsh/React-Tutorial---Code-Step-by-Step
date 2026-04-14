44c23e5674b0e4998bc99340c669ae5b5557b9de
Harsh Dhiman <harsh61355@gmail.com>
2026-04-14 23:46:41 +0530

    Layout and Index Routes in ReactJS

---
+++ src/41 Layout and Index Routes/Navbar.jsx
@@ -0,0 +1,19 @@
+import { Link, Outlet } from "react-router"
+
+const Navbar = () => {
+    return (
+        <div>
+            <div className="flex justify-between py-2 px-4">
+                <h1>Link</h1>
+                <div className="flex gap-4">
+                    <Link to="/" >Home</Link>
+                    <Link to="/about" >About</Link>
+                    <Link to="/college" >College</Link>
+                </div>
+            </div>
+            <Outlet />
+        </div>
+    )
+}
+
+export default Navbar
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './40 Nested Navigation with React Router/App.jsx'
+import App from './41 Layout and Index Routes/App.jsx'
 import { BrowserRouter } from 'react-router'
 
 createRoot(document.getElementById('root')).render(
