5604981d1748c52e747478b131b0cdd477730d85
Harsh Dhiman <harsh61355@gmail.com>
2026-04-28 13:22:41 +0530

    Route Prefixes in React Router DOM

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './41 Layout and Index Routes/App.jsx'
+import App from './43 Dynamic Routes/App.jsx'
 import { BrowserRouter } from 'react-router'
 
 createRoot(document.getElementById('root')).render(
