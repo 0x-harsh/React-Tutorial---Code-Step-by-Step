24bd937562dc9b069fbd60cf9a713148b08aa1dc
Harsh Dhiman <harsh61355@gmail.com>
2026-04-29 17:45:42 +0530

    Optional segment in react-router-dom

---
+++ src/44 Optional Segment/Login.jsx
@@ -0,0 +1,9 @@
+const Login = () => {
+    return (
+        <div style={{display: "flex", justifyContent: "center"}}>
+            <h1>Login Page</h1>
+        </div>
+    )
+}
+
+export default Login
---
+++ src/44 Optional Segment/Navbar.jsx
@@ -0,0 +1,22 @@
+import { Link, Outlet } from "react-router"
+
+const Navbar = () => {
+    return (
+        <div>
+            <div className="flex justify-between py-2 px-4">
+                <h1>Link</h1>
+                <div className="flex gap-4">
+                    <Link to="/" >Home</Link>
+                    <Link to="/about" >About</Link>
+                    <Link to="/college" >College</Link>
+                    <Link to="/in/user/login" >Login</Link>
+                    <Link to="/users" >Users</Link>
+                    <Link to="/users/list" >List</Link>
+                </div>
+            </div>
+            <Outlet /> {/* displays child routes data */}
+        </div>
+    )
+}
+
+export default Navbar
