9f0ceed454dd1a3e68fb51de5858cfe4a738502d
Harsh Dhiman <harsh61355@gmail.com>
2026-04-30 17:07:06 +0530

    installation of json-server and creation of api with endpoints

---
+++ src/47 install JSON Server and Make API/App.jsx
@@ -0,0 +1,11 @@
+// npm install json-server
+// create a db.json file
+// npx json-server db.json
+
+const App = () => {
+    return (
+        <div>App</div>
+    )
+}
+
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './46 API - Application Programming Interface/App.jsx'
+import App from './47 install JSON Server and Make API/App.jsx'
 import { BrowserRouter } from 'react-router'
 
 createRoot(document.getElementById('root')).render(
