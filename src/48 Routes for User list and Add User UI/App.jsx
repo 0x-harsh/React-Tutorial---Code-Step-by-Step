05820bd6bb46265f9104bdb1733a9d34e8f00f5a
Harsh Dhiman <harsh61355@gmail.com>
2026-05-04 15:20:13 +0530

    Using post method with api in react

--- src/48 Routes for User list and Add User UI/App.jsx
+++ src/48 Routes for User list and Add User UI/App.jsx
@@ -1,13 +1,14 @@
 import { Route, Routes } from "react-router"
 import Home from "./pages/Home"
 import Users from "./pages/Users"
+import AddUser from "./pages/AddUser"
 
 const App = () => {
     return (
         <Routes>
             <Route path="/" element={<Home/>} />
             <Route path="/users" element={<Users/>} />
-            <Route path="/add" element={<AddUsers/>} />
+            <Route path="/add" element={<AddUser/>} />
         </Routes>
     )
 }
--- src/48 Routes for User list and Add User UI/pages/AddUser.jsx
+++ src/48 Routes for User list and Add User UI/pages/AddUser.jsx
@@ -1,14 +1,33 @@
+import { useState } from "react"
 import { Link } from "react-router"
 
 const AddUser = () => {
+    const [name, setName] = useState('')
+    const [age, setAge] = useState('')
+    const [email, setEmail] = useState('')
+    const addUser = async () => {
+        const url = 'https://localhost:3000/users'
+        let response = await fetch(url, {
+            method: 'Post',
+            body: JSON.stringify({
+                "name": name,
+                "age": age,
+                "email": email
+            })
+        })
+        response = await response.json()
+        if(response) {
+            console.log(response)
+        }
+    }
     return (
         <div>
             <h1>ADD NEW USER</h1>
-            <div>
-                <input type="text" />
-                <input type="number" />
-                <input type="email" />
-                <button>Add</button>
+            <div className="w-100 flex flex-col gap-4 text-white">
+                <input onChange={(e) => { setName(e.target.value) }} type="text" className="rounded-lg bg-[#222] border border-neutral-600" />
+                <input onChange={(e) => { setAge(e.target.value) }} type="number" className="rounded-lg bg-[#222] border border-neutral-600" />
+                <input onChange={(e) => { setEmail(e.target.value) }} type="email" className="rounded-lg bg-[#222] border border-neutral-600" />
+                <button onClick={(e) => { addUser() }} className="rounded-lg bg-blue-700 font-medium" >Add</button>
             </div>
             <div>
                 <Link to="/" >Back to Home</Link>
